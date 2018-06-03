9469ab9a495b5cfec4c95cd3f91c6a69c5195133
x007xyz <1026161550@qq.com>
2018-06-03 18:20:27 +0800

    修改文件名

--- app/controller/generator.js
+++ app/controller/generator.js
@@ -26,34 +26,8 @@ class GeneratorController extends Controller {
     console.log(fileName);
     return await this.ctx.renderView(`components/${fileName}.html`, field);
   }
-  // async components() {
-  //   const body = this.ctx.request.body;
-  //   console.log(body);
-  //   const getTemplateVariable = function(tpl) {
-  //     const re = /\{\{\s*([a-zA-Z\.\_0-9()]+)\s*\}\}/m;
-  //     let match = re.exec(tpl);
-  //     const fields = [];
-  //     while (match) {
-  //       if (match.index > 0) {
-  //         fields.push({ name: match[1], explain: '' });
-  //       }
-  //       tpl = tpl.substring(match.index + match[0].length);
-  //       match = re.exec(tpl);
-  //     }
-  //     return fields;
-  //   };
-  //   const str = await this.ctx.renderView('fields.html', { fields: getTemplateVariable(body.content) });
-  //   console.log('str', str);
-  //   this.ctx.body = { content: str };
-  // }
-  // async save_components() {
-  //   console.log('components', this.ctx.request.body);
-  //   const { body } = this.ctx.request;
-  //   await this.ctx.model.Component.create(body);
-  //   await this.ctx.render('home.html', {});
-  // }
-  async create_form() {
-    await this.ctx.render('build_form.html', {});
+  async home() {
+    await this.ctx.render('home.html', {});
   }
   async processFields(fields, model = 'form') {
     for (const field of fields) {
@@ -62,7 +36,7 @@ class GeneratorController extends Controller {
       field.component = await this.getComponent(field);
     }
   }
-  async save_form() {
+  async save() {
     let result = { data: 'success' };
     const { content: fields, name } = this.ctx.request.body;
     if (Array.isArray(fields) && name !== '') {
@@ -75,25 +49,24 @@ class GeneratorController extends Controller {
   async resolve_form() {
     const fields = this.ctx.request.body.content;
     await this.processFields(fields);
-    const data = await this.ctx.renderView('form_components.html', { fields });
+    const data = await this.ctx.renderView('form_vue.html', { fields });
     this.ctx.body = { data };
   }
   async preview_form() {
     const fields = this.ctx.request.body.content;
     await this.processFields(fields);
-    const template = await this.ctx.renderView('form_components.html', { fields });
-    await this.ctx.render('iview_form.html', { template, fields });
+    await this.ctx.render('form_iview.html', { fields });
   }
   async resolve_table() {
     const fields = this.ctx.request.body.content;
     await this.processFields(fields, 'filter');
-    const data = await this.ctx.renderView('table.html', { fields });
+    const data = await this.ctx.renderView('table_vue.html', { fields });
     this.ctx.body = { data };
   }
   async preview_table() {
     const fields = this.ctx.request.body.content;
     await this.processFields(fields, 'filter');
-    await this.ctx.render('iview_table.html', { fields });
+    await this.ctx.render('table_iview.html', { fields });
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -5,13 +5,10 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
-  router.get('/form/create', controller.generator.create_form);
-  router.post('/form/preview', controller.generator.preview_form);
-  router.post('/form/save', controller.generator.save_form);
+  router.get('/', controller.generator.home);
+  router.post('/save', controller.generator.save);
   router.post('/form/resolve', controller.generator.resolve_form);
+  router.post('/form/preview', controller.generator.preview_form);
   router.post('/table/resolve', controller.generator.resolve_table);
   router.post('/table/preview', controller.generator.preview_table);
-  // router.post('/generator/components', controller.generator.components);
-  // router.post('/generator/components/save', controller.generator.save_components);
 };
